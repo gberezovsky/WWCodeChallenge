f0b9c424ba3d4e114a10251d32bdfd2f0ddfaa64
gberezovsky <guy.berezovsky@gmail.com>
2019-02-09 16:18:01 -0800

    Solution to question #1

---
+++ question1.js
@@ -0,0 +1,30 @@
+
+// Read file content and print word n meaning
+function doesFileExist (filePath) {
+    const fs = require('fs') 
+
+    fs.readFile(filePath, 'utf8', (err, data) => { 
+        if (err) {
+            console.log('File cannot be found')
+        } else {
+            let lines = data.split('\n')
+            for (let i = 0; i < lines.length; i++) {
+                // console.log(lines[j])
+                let singleLine = lines[i].split('-')
+                console.log(singleLine[0])
+                if (singleLine[1]) {
+                    let definition = singleLine[1].split(',')
+                    for (let j = 0; j < definition.length; j++) {
+                        console.log(definition[j])
+                    }
+                }
+            }
+        }
+      
+    })
+}
+
+doesFileExist("./data.txt")
+
+
+
